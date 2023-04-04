710dd3490a3759ffb5bfd9f12c55c15ce7bf3dee
Evelina Norlin <evelina.norlin@hotmail.com>
2023-04-04 09:21:21 +0200

    added editNote.js

--- frontend/script/editNote.js
+++ frontend/script/editNote.js
@@ -1,3 +1,5 @@
+import { renderNotes } from "./showNotes.js";
+
 let notesContainer = document.getElementById('notes');
 
 export function editNote(noteId){
@@ -9,8 +11,10 @@ export function editNote(noteId){
           notesContainer.innerHTML = `
           <button id="backBtn">Go back</button>
           <h2>Edit "${data.title}"</h2>
-          <p>description:</p>
-          <input type="text" value="${data.description}">
+          <label for="titleinput">Title:</label>
+          <input type="text" name="titleinput" value="${data.title}" id="titleInput">
+          <label for="descriptioninput">Description:</label>
+          <input type="text" name="descriptioninput" value="${data.description}" id="descInput">
           <textarea id="mytextarea">${data.content}</textarea>
           <button id="saveBtn">save changes</button>
           `;
@@ -24,8 +28,37 @@ export function editNote(noteId){
               mytextarea.value = contentValue;
             });
           }
-        });
-        }
-      })
+        })
+        const titleInput = document.getElementById('titleInput');
+        const descInput = document.getElementById('descInput');
+        
+        document.getElementById('saveBtn').addEventListener('click', () => {
+          let author = localStorage.getItem('username')
+          let note = {
+            "id": noteId,
+            "title": titleInput.value,
+            "description": descInput.value,
+            "content": mytextarea.value,
+            "author": author
+          }
+
+          fetch('http://localhost:3000/notes/edit', {
+            method: "PUT",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify(note)
+          })
+            .then(res => res.json())
+            .then(data => {
+              notesContainer.innerHTML = `
+              <h3>The note is updated!</h3>
+              <button id="backBtn">Back to notes</button>`
+              
+              document.getElementById('backBtn').addEventListener('click', renderNotes)
+          })
+        })
+      }
     })
+  })
 }
